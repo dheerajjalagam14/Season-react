bcf7d9ca8b9730c16f831c2164d99cde2cdb941f
Dheeraj <Dheeraj@Users-MacBook-Pro.local>
2019-03-06 20:57:25 -0600

    Completed with successful location based advice

--- src/index.js
+++ src/index.js
@@ -1,23 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Loader from './Loader';
 
 
-class App extends React.Component{
+class App extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = { lat : null };
+  state={ lat: null, errorMessage:'' };
 
+  componentDidMount(){
     navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err)=> console.log(err)
+      (position) => this.setState({lat: position.coords.latitude}),
+      (err) =>  this.setState({errorMessage: err.message})
     );
   }
 
-  render(){
-    return <div>Latitude:{this.state.lat}</div>;
+  renderContent(){
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div > Error: {
+        this.state.errorMessage
+      } < /div>
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat}/>
+    }
+
+    return <Loader message="Please accept location request"/>;
   }
+
+  render() {
+    return (
+      <div>{this.renderContent()}</div>
+    );
+  }
+
 }
-ReactDOM.render(<App/>,document.querySelector("#root"));
+
+
+ReactDOM.render( < App /  > , document.querySelector("#root"));
